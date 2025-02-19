7272d3b9444f7ab082fffc31561a5a3b3e4dd587
KorneliaBlaszczuk <kornelia.blaszczuk@onet.pl>
2025-02-19 15:03:01 +0100

    using database

--- timer.js
+++ timer.js
@@ -1,14 +1,21 @@
 let countdown;
-let totalTime = 300;
 const timerDisplay = document.getElementById("timer");
 const alarmSound = document.getElementById("alarm-sound");
 const urlParams = new URLSearchParams(window.location.search);
 const task = urlParams.get('task') || "Timer";
-document.getElementById("task-title").textContent = task;
+const time = urlParams.get('time') || 10;
+
+let totalTime = time * 60;
+
+function displayName(task) {
+    const taskTitle = document.getElementById('task-title');
+    taskTitle.textContent = task;
+}
+
+displayName(task);
 
 // Update display to show the current time
 function updateDisplay(seconds) {
-    console.log("Hello");
     const min = Math.floor(seconds / 60);
     const sec = seconds % 60;
     timerDisplay.textContent = `${String(min).padStart(2, '0')}:${String(sec).padStart(2, '0')}`;
@@ -38,7 +45,7 @@ function stopTimer() {
 // Reset the timer to 5 minutes
 function resetTimer() {
     clearInterval(countdown);
-    totalTime = 300;
+    totalTime = 600;
     updateDisplay(totalTime);
 }
 
